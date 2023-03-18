8ab19959cc62a9e2aa86bf5fc9925c63aa60c151
221rdb087 <125808567+221rdb087@users.noreply.github.com>
2023-03-18 15:39:44 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -7,16 +7,15 @@ def build_heap(data):
             k = 2*j + 1
             if k >= n:
                 break
-            if k+1 < n and data[k+1] > data[k]:
+            if k+1 < n and data[k+1] < data[k]:
                 k += 1
-            if data[k] > data[j]:
+            if data[k] < data[j]:
                 data[j], data[k] = data[k], data[j]
                 swaps.append((j, k))
                 j = k
             else:
                 break
     return swaps
-
 
 def heap_sort(data):
     swaps = build_heap(data)
@@ -28,16 +27,15 @@ def heap_sort(data):
             k = 2*j + 1
             if k >= i:
                 break
-            if k+1 < i and data[k+1] > data[k]:
+            if k+1 < i and data[k+1] < data[k]:
                 k += 1
-            if data[k] > data[j]:
+            if data[k] < data[j]:
                 data[j], data[k] = data[k], data[j]
                 swaps.append((j, k))
                 j = k
             else:
                 break
     return swaps
-
 
 def main():
 
@@ -63,7 +61,6 @@ def main():
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
-
 
 if __name__ == "__main__":
     main()
